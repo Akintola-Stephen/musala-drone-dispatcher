be585768c0d52c31503bfe46dca285f793bd3f59
Akintola-Stephen <sakintola351@gmail.com>
2024-02-21 15:09:34 +0100

    Added: Drone repository with methods

--- drone-dispatcher/drone-dispatcher/src/main/java/com/musala/dronedispatcher/Drone/repository/DroneRepository.java
+++ drone-dispatcher/drone-dispatcher/src/main/java/com/musala/dronedispatcher/Drone/repository/DroneRepository.java
@@ -1,4 +1,19 @@
 package com.musala.dronedispatcher.Drone.repository;
 
-public interface DroneRepository {
+import com.musala.dronedispatcher.Drone.domain.Drone;
+import com.musala.dronedispatcher.Drone.domain.enums.State;
+import com.musala.dronedispatcher.Drone.dto.DroneDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DroneRepository extends JpaRepository<Drone, String>{
+  List<Drone>  getAllDrones();
+
+  Integer getCapacityForSerial(String serialNumber);
+
+  List<Drone>  getDroneByState(State state);
+
+  Drone registerDrone(DroneDto droneDto);
+
 }
