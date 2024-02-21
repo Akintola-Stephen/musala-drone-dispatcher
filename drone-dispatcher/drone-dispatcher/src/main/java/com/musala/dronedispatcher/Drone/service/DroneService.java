11d6f29d4b9de21e2165ee6dc6d03161af4c600b
Akintola-Stephen <sakintola351@gmail.com>
2024-02-21 15:11:56 +0100

    Added: A service with methods implementations

--- drone-dispatcher/drone-dispatcher/src/main/java/com/musala/dronedispatcher/Drone/service/DroneService.java
+++ drone-dispatcher/drone-dispatcher/src/main/java/com/musala/dronedispatcher/Drone/service/DroneService.java
@@ -1,2 +1,37 @@
-package com.musala.dronedispatcher.Drone.service;public class DroneService {
+package com.musala.dronedispatcher.Drone.service;
+
+import com.musala.dronedispatcher.Drone.domain.Drone;
+import com.musala.dronedispatcher.Drone.domain.enums.State;
+import com.musala.dronedispatcher.Drone.dto.DroneDto;
+import com.musala.dronedispatcher.Drone.repository.DroneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public abstract class DroneService implements DroneRepository {
+
+  @Autowired
+  DroneRepository droneRepository;
+
+
+  @Override
+  public List<Drone> getAllDrones() {
+    return droneRepository.getAllDrones();
+  }
+
+  @Override
+  public Integer getCapacityForSerial(String serialNumber) {
+    return droneRepository.getCapacityForSerial(serialNumber);
+  }
+
+  @Override
+  public List<Drone> getDroneByState(State state) {
+    return droneRepository.getDroneByState(state);
+  }
+
+  @Override
+  public Drone registerDrone(DroneDto droneDto) {
+    return droneRepository.registerDrone(droneDto);
+  }
+
 }
